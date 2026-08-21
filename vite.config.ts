96aa64ec342d394620c1ed7d2a3c64b77cf238c7
onesip <info@onesip.nl>
2026-08-21 22:57:37 +0200

    Register weekend responsibility content update

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { strictBreakSchedulePlugin } from './plugins/strictBreakSchedulePlugin';
 import { breakEdgeCasePlugin } from './plugins/breakEdgeCasePlugin';
 import { staffRulesPopupPlugin } from './plugins/staffRulesPopupPlugin';
 import { staffRulesTdzFixPlugin } from './plugins/staffRulesTdzFixPlugin';
+import { weekendResponsibilityContentPlugin } from './plugins/weekendResponsibilityContentPlugin';
 import { weekendResponsibilityMountPlugin } from './plugins/weekendResponsibilityMountPlugin';
 import { runtimeStaffSmokePlugin } from './plugins/runtimeStaffSmokePlugin';
 import { runtimeManagerSmokePlugin } from './plugins/runtimeManagerSmokePlugin';
@@ -19,6 +20,7 @@ export default defineConfig({
     breakEdgeCasePlugin(),
     staffRulesPopupPlugin(),
     staffRulesTdzFixPlugin(),
+    weekendResponsibilityContentPlugin(),
     weekendResponsibilityMountPlugin(),
     runtimeStaffSmokePlugin(),
     runtimeManagerSmokePlugin(),
