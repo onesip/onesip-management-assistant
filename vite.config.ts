845feb37b06b9f6764dcf4a4ecf4fb30c866f683
onesip <info@onesip.nl>
2026-08-21 15:49:40 +0200

    Register weekend shift responsibility plugin

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { mandatoryBreakRulesPlugin } from './plugins/mandatoryBreakRulesPlugin';
 import { strictBreakSchedulePlugin } from './plugins/strictBreakSchedulePlugin';
 import { breakEdgeCasePlugin } from './plugins/breakEdgeCasePlugin';
 import { staffRulesPopupPlugin } from './plugins/staffRulesPopupPlugin';
+import { weekendShiftResponsibilityPlugin } from './plugins/weekendShiftResponsibilityPlugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,6 +15,7 @@ export default defineConfig({
     strictBreakSchedulePlugin(),
     breakEdgeCasePlugin(),
     staffRulesPopupPlugin(),
+    weekendShiftResponsibilityPlugin(),
     react(),
     VitePWA({
       registerType: 'autoUpdate',
